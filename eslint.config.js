a3e54467fc9c1e9437059fadc05290156a72ed5d
zellship <raul@zellship.com>
2026-09-08 10:33:55 -0600

    chore: exclude published cuts from source checks

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "dist-pages", ".output", ".vinxi"] },
+  { ignores: ["dist", "dist-pages", "public-experiences", ".output", ".vinxi"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
